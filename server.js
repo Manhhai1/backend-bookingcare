a1a3bc806258ca2d5ba303c2bcc48f745ca89516
HaiManh <vomanhhai230720@gmail.com>
2023-02-18 16:54:41 +0000

    update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 initWebRouter(app)
 connectDB()
-let port = process.env.PORT || 6060
+let port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log("Backend NodeJs running on the port: " + port)
 })
